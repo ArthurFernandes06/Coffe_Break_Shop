a0e3a2122e9da7b1abd8bc84fb50c37c668ce65c
lucas <lucasvbento1@gmail.com>
2026-03-21 19:35:35 -0300

    Configurar os arquivos para usar java web MVC

---
+++ backend/src/main/java/com/coffebreak/util/CorsFilter.java
@@ -0,0 +1,17 @@
+package com.coffebreak.util;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        chain.doFilter(req, res);
+    }
+}
