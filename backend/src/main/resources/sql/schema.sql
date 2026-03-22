be7be371718f0da5997634df78a22a1dadab528b
Caio Lucas <caiolucasprogramador@gmail.com>
2026-03-22 20:15:46 -0300

    feat: estrutura do banco de dados

---
+++ backend/src/main/resources/sql/schema.sql
@@ -0,0 +1,65 @@
+create table usuario (
+	id serial primary key, 
+	nome varchar(100) not null,
+	email varchar(100) unique not null,
+	endereco varchar(150),
+	senha varchar(255),
+	tipo boolean default false
+);
+
+create table venda (
+	id serial primary key,
+	id_usuario int references usuario(id),
+	data_hora timestamptz default current_timestamp,
+	valor_total decimal(10,2),
+	status varchar(30) default 'PENDENTE',
+	
+	constraint CHK_status check (status in (
+        'PENDENTE', 
+        'PAGO',  
+        'ENVIADO',  
+        'CANCELADO'
+    ))
+);
+
+create table categoria (
+	id serial primary key,
+	nome varchar(50) unique not null,
+	descricao varchar(150)
+);
+
+create table produto (
+	id serial primary key,
+	nome varchar(100) not null,
+	preco decimal(10,2) not null,
+	foto TEXT,
+	descricao varchar(255),
+	quantidade int default 0,
+	id_categoria int references categoria(id)
+);
+
+create table venda_produto (
+	id_venda int references venda(id),
+	id_produto int references produto(id),
+	quantidade int not null,
+	preco_unitario decimal(10,2) not null,
+	
+	primary key (id_venda, id_produto)
+);
+
+create table carrinho (
+	id serial primary key,
+	id_usuario int references usuario(id),
+	status varchar(10),
+	data_criacao timestamptz default current_timestamp,
+	
+	constraint CHK_status check (status in ('ATIVO', 'INATIVO', 'ABANDONADO'))
+);
+
+create table carrinho_produto (
+	id_carrinho int references carrinho(id),
+	id_produto int references produto(id),
+	quantidade int not null,
+	
+	primary key (id_carrinho, id_produto)
+);
