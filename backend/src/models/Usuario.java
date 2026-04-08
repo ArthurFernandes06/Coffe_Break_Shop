8f4a64f4819b10937a733debf4cc8ac37ccbce7a
Caio Lucas <caiolucasprogramador@gmail.com>
2026-04-08 17:13:47 -0300

    feat: cria pasta models e classe usuario

---
+++ backend/src/models/Usuario.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private boolean tipo;
+    
+    public List<Usuario> obterTodos() {
+        List<Usuario>;
+    }
+}
+
