9e25ac2d1037b3c0a3458c0cc2bb75fff1242fed
Caio Lucas <caiolucasprogramador@gmail.com>
2026-04-12 20:03:04 -0300

    feat: adicionar interatividade na aba Meu Perfil

--- frontend/pagina_do_usuario/script.js
+++ frontend/pagina_do_usuario/script.js
@@ -9,3 +9,43 @@ links.forEach(link => {
         document.getElementById(clickedsection).style.display = "block"
     })
 })
+
+const btnsEdit = document.querySelectorAll(".editar")
+
+btnsEdit.forEach(btn => {
+    btn.addEventListener("click", (event) => {
+        const clickedbtn = event.target.dataset.campo
+        
+        const input = document.getElementById(`input-${clickedbtn}`)
+        const label = document.querySelector(`label[for="input-${clickedbtn}"]`)
+        const btnSalvar = document.getElementById(`btn-salvar-${clickedbtn}`)
+
+        input.style.display = "block"
+        label.style.display = "block"
+
+        btnSalvar.style.display = "block"
+        btn.style.display = "none"
+    })
+})
+
+const btnSave = document.querySelectorAll(".salvar")
+
+btnSave.forEach(btn => {
+    btn.addEventListener("click", (event) => {
+        const clickedbtn = event.target.dataset.campo
+
+        const input = document.getElementById(`input-${clickedbtn}`)
+        const label = document.querySelector(`label[for="input-${clickedbtn}"]`)
+        const valor = input.value 
+        const btnEditar = document.getElementById(`btn-editar-${clickedbtn}`)
+
+        const titulo = document.getElementById(`exibir-${clickedbtn}`)
+        titulo.innerHTML = valor
+
+        input.style.display = "none"
+        label.style.display = "none"
+
+        btnEditar.style.display = "block"
+        btn.style.display = "none"
+    })
+})
