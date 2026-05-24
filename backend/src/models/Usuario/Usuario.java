4738f7dda3980bdabe83db273aec11c265e126d8
Caio Lucas <caiolucasprogramador@gmail.com>
2026-05-24 20:12:17 -0300

    feat: add entidade Usuario

--- backend/src/models/Usuario/Usuario.java
+++ backend/src/models/Usuario/Usuario.java
@@ -1,9 +1,5 @@
 package Usuario;
 
-import java.util.List;
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 public class Usuario {
     private int id;
     private String nome;
@@ -11,8 +7,39 @@ public class Usuario {
     private String senha;
     private boolean tipo;
 
-    public List<Usuario> obterTodos() {
-        List<Usuario>
+    public Usuario() {};
+
+    public Usuario(int id, String nome, String email, String senha, boolean tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+    public Usuario(String nome, String email, String senha, boolean tipo) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+    public int getId() {return this.id;}
+    public void setId(int id) {this.id = id;}
+
+    public String getNome() {return this.nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getEmail() {return this.email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getSenha() {return this.senha;}
+    public void setSenha(String senha) {this.senha = senha;}
+
+    public boolean isTipo() {return this.tipo;}
+    public void setTipo(boolean tipo) {this.tipo = tipo;}
+
+    @Override
+    public String toString() {
+        return "ID: " + this.id + " | NOME: " + this.nome;
     }
 }
 
