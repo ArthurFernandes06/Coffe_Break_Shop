3c8e603b24d460df86a2d8ce74f0d4a8970d85a8
Caio Lucas <caiolucasprogramador@gmail.com>
2026-05-28 15:40:44 -0300

    feat: add entidade CarrinhoProduto

---
+++ backend/src/main/java/com/coffeebreak/models/carrinhoproduto/CarrinhoProduto.java
@@ -0,0 +1,45 @@
+package carrinhoproduto;
+
+import Carrinho.Carrinho;
+import Produto.Produto;
+
+public class CarrinhoProduto {
+    private Carrinho carrinho;
+    private Produto produto;
+    private int quantidade;
+
+    public CarrinhoProduto(Carrinho carrinho, Produto produto, int quantidade) {
+        this.carrinho = carrinho;
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Carrinho getCarrinho() {
+        return carrinho;
+    }
+
+    public void setCarrinho(Carrinho carrinho) {
+        this.carrinho = carrinho;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Carrinho: " + this.carrinho.getId() + " | Produto: " + this.produto.getNome() + " | Quantidade: " + this.quantidade;
+    }
+}
