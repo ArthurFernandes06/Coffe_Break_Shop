7d85a88adde103fc108508b82178a85d04721a63
Caio Lucas <caiolucasprogramador@gmail.com>
2026-05-28 09:28:47 -0300

    feat: add entidade Venda

---
+++ backend/src/main/java/com/coffeebreak/models/Venda/Venda.java
@@ -0,0 +1,63 @@
+package Venda;
+
+import Usuario.Usuario;
+
+import java.sql.Timestamp;
+
+public class Venda {
+    private int id;
+    private Timestamp dataHora;
+    private double valorTotal;
+    private String status; // PENDENTE, PAGO, ENVIADO, CANCELADO
+    private Usuario usuario;
+
+    public Venda(int id, double valorTotal, String status, Usuario usuario) {
+        this.id = id;
+        this.valorTotal = valorTotal;
+        this.status = status;
+        this.usuario = usuario;
+    }
+
+    public Venda(double valorTotal, String status, Usuario usuario) {
+        this.valorTotal = valorTotal;
+        this.status = status;
+        this.usuario = usuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Timestamp getDataHora() {
+        return dataHora;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + this.id + " | Usuario: " + this.usuario.getNome() + " | Total: R$" + this.valorTotal + " | Status: " + this.status;
+    }
+}
