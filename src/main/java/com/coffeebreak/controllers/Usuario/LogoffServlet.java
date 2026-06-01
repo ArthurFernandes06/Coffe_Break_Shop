6a904e6d3ba77db0975202ff81800ef1ebb41aaa
Caio Lucas <caiolucasprogramador@gmail.com>
2026-06-01 11:14:39 -0300

    feat: add LogoffServlet

---
+++ src/main/java/com/coffeebreak/controllers/Usuario/LogoffServlet.java
@@ -0,0 +1,24 @@
+package com.coffeebreak.controllers.Usuario;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/logoff")
+public class LogoffServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession sessao = request.getSession(false);
+        if (sessao != null) {
+            sessao.invalidate();
+        }
+        response.sendRedirect(request.getContextPath() + "/login");
+    }
+}
