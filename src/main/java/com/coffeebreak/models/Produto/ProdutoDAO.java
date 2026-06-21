d384df8e7579f4b246b7c9ab66855f53c4ea5473
Caio Lucas <caiolucasprogramador@gmail.com>
2026-06-21 13:14:43 -0300

    feat: add contarTodos em ProdutoDAO

--- src/main/java/com/coffeebreak/models/Produto/ProdutoDAO.java
+++ src/main/java/com/coffeebreak/models/Produto/ProdutoDAO.java
@@ -115,4 +115,20 @@ public class ProdutoDAO {
         }
         return resultado;
     }
+
+    public int contarTodos() {
+        String sql = "SELECT COUNT(*) as total FROM produto";
+
+        try (Connection conn = Conexao.getConexao();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("total");
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return 0;
+    }
 }
