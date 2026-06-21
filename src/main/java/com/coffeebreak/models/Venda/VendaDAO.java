17691afd923764d9ddbd92b051c5ddb3318cf16e
Caio Lucas <caiolucasprogramador@gmail.com>
2026-06-21 20:55:30 -0300

    fix: Conserta nome da coluna status_pedido em VendaDAO

--- src/main/java/com/coffeebreak/models/Venda/VendaDAO.java
+++ src/main/java/com/coffeebreak/models/Venda/VendaDAO.java
@@ -2,10 +2,7 @@ package com.coffeebreak.models.Venda;
 
 import com.coffeebreak.models.Usuario.*;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +12,27 @@ public class VendaDAO {
     private Venda mapearVenda(ResultSet rs) throws SQLException {
         UsuarioDAO usuarioDAO = new UsuarioDAO();
         Usuario usuario = usuarioDAO.obterPeloId(rs.getInt("id_usuario"));
-        return new Venda(rs.getInt("id"), rs.getTimestamp("data_hora"), rs.getDouble("valor_total"), rs.getString("status"), usuario);
+        return new Venda(rs.getInt("id"), rs.getTimestamp("data_hora"), rs.getDouble("valor_total"), rs.getString("status_pedido"), usuario);
     }
 
+    public List<Venda> obterTodas() {
+        List<Venda> resultado = new ArrayList<Venda>();
+        String sql = "SELECT * FROM venda";
+
+        try (Connection conn = Conexao.getConexao();
+             Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+
+            while (rs.next()) {
+                resultado.add(mapearVenda(rs));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return resultado;
+    }
     public boolean criarVenda(Venda venda) {
-        String sql = "INSERT INTO venda(id_usuario, valor_total, status) values(?, ?, ?)";
+        String sql = "INSERT INTO venda(id_usuario, valor_total, status_pedido) values(?, ?, ?)";
         try (Connection conn = Conexao.getConexao();
             PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setInt(1, venda.getUsuario().getId());
@@ -49,7 +62,7 @@ public class VendaDAO {
     }
 
     public boolean atualizarStatus(int id, String status) {
-        String sql = "UPDATE venda SET status = ? WHERE id = ?";
+        String sql = "UPDATE venda SET status_pedido = ? WHERE id = ?";
         try (Connection conn = Conexao.getConexao();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, status);
@@ -79,7 +92,7 @@ public class VendaDAO {
 
     public List<Venda> obterPorStatus(String status) {
         List<Venda> resultado = new ArrayList<Venda>();
-        String sql = "SELECT * FROM venda WHERE status = ?";
+        String sql = "SELECT * FROM venda WHERE status_pedido = ?";
         try (Connection conn = Conexao.getConexao();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, status);
