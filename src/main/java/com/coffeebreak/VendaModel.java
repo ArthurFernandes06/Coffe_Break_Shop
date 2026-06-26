fe7a3162e4f91b483659ca2a2be6f5b5ab824458
ArthurFernandes06 <artrdgs@gmail.com>
2026-06-25 21:07:49 -0300

    Removendo código desnecessário

--- src/main/java/com/coffeebreak/VendaModel.java
+++
@@ -1,4 +0,0 @@
-package com.coffeebreak;
-public class VendaModel {
-    
-}
--- src/main/java/com/coffeebreak/controllers/VendaController.java
+++
@@ -1,5 +0,0 @@
-package com.coffeebreak.controllers;
-//Os Controllers recebem as requisições e chamam os repositories.
-public class VendaController {
-    
-}
